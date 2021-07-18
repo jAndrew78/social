f89360e37674ac7e904799d7b8f1e563c7c3540e
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-07-18 13:26:08 -0500

    copied all from SignIn, added AuthContainer & state variables for username

--- src/screens/SignUpScreen/index.jsx
+++ src/screens/SignUpScreen/index.jsx
@@ -1,18 +1,99 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  Container,
+  Main,
+  Auth,
+  AuthContainer,
+  AuthTitle,
+  AuthField,
+  SignUpContainer,
+  SignIn,
+  Loading,
+  Background,
+  StatusBar,
+} from "./styles/SignUpScreen";
 
-export default SignUpScreen = () => {
+import BgImage from '../../../assets/SignInBg.png'
+import { Text } from '../../components';
+
+export default SignUpScreen = ({navigation}) => {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Sign Up Screen</Text>
-    </View>
+    <Container>
+      
+      <Main>
+        <Text center title semi>
+          Come and join us
+        </Text>
+      </Main>
+
+      <Auth>
+
+        <AuthContainer>
+          <AuthTitle small semi>Username</AuthTitle>
+          <AuthField 
+            autoCapitalize="none" 
+            autoCorrect={false} 
+            autoFocus={false}
+            onChangeText={username => setUsername(username.trim())}
+            value={username}
+          />
+        </AuthContainer>
+
+        <AuthContainer>
+          <AuthTitle small semi>Email Address</AuthTitle>
+          <AuthField 
+            autoCapitalize="none" 
+            autoCompleteType="email" 
+            autoCorrect={false} 
+            autoFocus={false}
+            keyboardType="email-address"
+            onChangeText={email => setEmail(email.trim())}
+            value={email}
+          />
+        </AuthContainer>
+
+        <AuthContainer>
+          <AuthTitle small semi>Password</AuthTitle>
+          <AuthField  
+            autoCapitalize="none" 
+            autoCompleteType="password" 
+            autoCorrect={false} 
+            autoFocus={false}
+            secureTextEntry={true}
+            onChangeText={password => setPassword(password.trim())}
+            value={password}
+          />
+        </AuthContainer>
+
+      </Auth>
+
+      <SignUpContainer disabled={loading}>
+        {loading ? (
+          <Loading />
+        ) : (
+          <Text center bold medium white>
+            Sign Up
+          </Text>
+        )}
+      </SignUpContainer>
+
+      <SignIn onPress={() => navigation.navigate('SignIn')}>
+        <Text center small> Already Social?{' '} 
+          <Text violet small>
+            Sign In
+          </Text>
+        </Text>
+      </SignIn>
+      
+      <Background source={BgImage}>
+      </Background>
+      
+      <StatusBar barStyle="light-content" />
+    </Container>    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-});
--- src/screens/SignUpScreen/styles/SignUpScreen.js
+++ src/screens/SignUpScreen/styles/SignUpScreen.js
@@ -1,5 +1,57 @@
 import styled from 'styled-components';
+import { Text } from '../../../components';
 
 export const Container = styled.View`
   flex: 1;
 `;
+
+export const Main = styled.View`
+  margin-top: 192px;
+`;
+
+export const Auth = styled.View`
+  margin: 64px 32px 32px; 
+`;
+
+export const AuthContainer = styled.View`
+  margin-bottom: 32px;
+`;
+
+export const AuthTitle = styled(Text)`
+  color: #333;
+  text-transform: uppercase;
+`;
+
+export const AuthField = styled.TextInput`
+  border-bottom-color: #333;
+  border-bottom-width: .5px;
+  height: 48px;
+  font-size: 24px;
+`;
+
+export const SignUpContainer = styled.TouchableOpacity`
+  margin: 0 32px;
+  height: 48px;
+  align-items: center;
+  justify-content: center;
+  background-color: #919dc2;
+  border-radius: 10px;
+`;
+
+export const SignIn = styled.TouchableOpacity`
+  margin-top: 16px;
+`;
+
+export const Loading = styled.ActivityIndicator.attrs(props => ({
+  color: "#fff",
+  size: "small",
+}))``;
+
+export const Background = styled.ImageBackground`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -100;
+`;
+
+export const StatusBar = styled.StatusBar``;
