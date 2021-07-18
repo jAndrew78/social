9231f0010d614abb8fc22c52b0bbe9d5d5506465
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-07-18 17:27:30 -0500

    created config/firebase.js and context/FirebaseContext.js

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBRSgwVZdyihwsAkMflIqep6tU2ybGKzPg",
+    authDomain: "social-cb411.firebaseapp.com",
+    databaseURL: "https://social-cb411.firebaseio.com",
+    projectId: "social-cb411",
+    storageBucket: "social-cb411.appspot.com",
+    messagingSenderId: "228535715479",
+    appId: "1:228535715479:web:c6ef93e84fd7c679c94bd2"
+}
