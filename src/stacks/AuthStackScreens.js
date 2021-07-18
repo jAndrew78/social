c4c22b04c8c88a81a4b4fd51a66ed9e650cb3a18
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-07-17 23:01:01 -0500

    created basic signIn/signUp screens and imported through Auth/AppStackScreens

--- src/stacks/AuthStackScreens.js
+++ src/stacks/AuthStackScreens.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { SignInScreen, SignUpScreen } from '../screens'
+
 export default AuthStackScreens = () => {
   const AuthStack = createStackNavigator();
 
   return (
     <AuthStack.Navigator headerMode="none">
-
+      <AuthStack.Screen name="SignIn" component={SignInScreen} />
+      <AuthStack.Screen name="SignUp" component={SignUpScreen} />
     </AuthStack.Navigator>
   );
 }
