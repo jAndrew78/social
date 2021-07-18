d425270b640aaea77e0180c5a398977b1cd2679a
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-07-18 16:25:18 -0500

    wrapped all in UserProvider in App.js (in previous commit - committed before comment was fin)

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 
-import { UserProvider } from './src/context/UserContext';
+import { UserProvider } from './src/context/UserContext'; 
 import AppStackScreens from './src/stacks/AppStackScreens';
 
 export default App = () => {
   return (
-    <UserProvider>
-      <NavigationContainer>
-        <AppStackScreens />
-      </NavigationContainer>
-    </UserProvider>
+    <UserProvider> 
+      <NavigationContainer> 
+        <AppStackScreens /> 
+      </NavigationContainer> 
+    </UserProvider> 
   )
 }
