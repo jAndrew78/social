46200919d1bca9ccc12f73601218df84c40389a9
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-07-17 22:41:50 -0500

    created basic stack files

---
+++ src/stacks/AppStackScreens.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export default AppStackScreens = () => {
+  const AppStack = createStackNavigator();
+
+  return (
+    <AppStack.Navigator headerMode="none">
+
+    </AppStack.Navigator>
+  );
+}
