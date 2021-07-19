f89a2c008ab093bde73ef2b344205744a7feb3dd
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-07-18 21:26:11 -0500

    set AppStack to show LoadingScreen before AuthScreens

--- src/stacks/AppStackScreens.js
+++ src/stacks/AppStackScreens.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { UserContext } from "../context/UserContext";
 import AuthStackScreens from './AuthStackScreens';
 import MainStackScreens from './MainStackScreens';
+import { LoadingScreen } from '../screens';
 
 export default AppStackScreens = () => {
   const AppStack = createStackNavigator();
@@ -11,7 +12,9 @@ export default AppStackScreens = () => {
 
   return (
     <AppStack.Navigator headerMode="none">
-      {user.isLoggedIn ? (
+      {user.isLoggedIn === null ? (
+        <AppStack.Screen name="Loading" component={LoadingScreen} />
+      ) : user.isLoggedIn ? (
         <AppStack.Screen name="Main" component={MainStackScreens} />
       ) : (
         <AppStack.Screen name="Auth" component={AuthStackScreens} />
